d616bd7287d27c7fb259bc6da48f1d9d02fc748c
sergiocastillero <cf17sergio.castillero@iesjoandaustria.org>
2021-11-23 19:47:16 +0000

    [JS-02] add button facebook in login screen, conected prelogin with login and start working with Firebase

--- JaEstic_user/app/src/main/java/com/grup2/jaestic_user/LoginScreen.java
+++ JaEstic_user/app/src/main/java/com/grup2/jaestic_user/LoginScreen.java
@@ -1,14 +1,34 @@
 package com.grup2.jaestic_user;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+        Intent redirect = new Intent(this, LoginScreen.class);
+
+        EditText txtMail = findViewById(R.id.txtMail);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+        Button btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (txtMail.getText().toString().equals("sergio") && txtPassword.getText().toString().equals("sergio")) {
+                    startActivity(redirect);
+                }
+            }
+        });
     }
 }
--- JaEstic_user/app/src/main/java/com/grup2/jaestic_user/PreLoginScreen.java
+++ JaEstic_user/app/src/main/java/com/grup2/jaestic_user/PreLoginScreen.java
@@ -2,7 +2,11 @@ package com.grup2.jaestic_user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PreLoginScreen extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class PreLoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prelogin_screen);
+        Intent redirect = new Intent(this, LoginScreen.class);
+        Intent redirect2 = new Intent(this, RegisterScreen.class);
+
+        Button btnsignin = findViewById(R.id.btnsignin);
+        Button btnsignup = findViewById(R.id.btnsignup);
+
+        btnsignin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(redirect);
+            }
+        });
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(redirect2);
+            }
+        });
     }
 }
