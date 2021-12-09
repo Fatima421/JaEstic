eb2bf4565f5b2bd7f759666b16a2618223f017f1
Jordi Guzmán <cf18jordi.guzman@iesjoandaustria.org>
2021-12-09 18:28:18 +0100

    More files

--- JaEstic_user/app/src/main/java/com/grup2/jaestic_user/RecyclerViewAdapters/CartRecyclerViewAdapter.java
+++ JaEstic_user/app/src/main/java/com/grup2/jaestic_user/RecyclerViewAdapters/CartRecyclerViewAdapter.java
@@ -1,56 +1,75 @@
 package com.grup2.jaestic_user.RecyclerViewAdapters;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.grup2.jaestic_user.Fragments.DishesListFragment;
+import com.grup2.jaestic_user.Models.Category;
 import com.grup2.jaestic_user.R;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
-    private ArrayList<String> array_noms;
+public class CartRecyclerViewAdapter extends RecyclerView.Adapter<CartRecyclerViewAdapter.CategoryViewHolder> {
+    // Properties
+    private ArrayList<Category> arrayCategories;
+    Context context;
 
-    public RecyclerViewAdapter(ArrayList<String> arrN){
-        array_noms = arrN;
+    // Constructor
+    public CartRecyclerViewAdapter(Context context, ArrayList<Category> arrayCategories){
+        this.context = context;
+        this.arrayCategories = arrayCategories;
     }
 
+    // App LifeCycle
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart, parent, false);
-        ViewHolder holder = new ViewHolder(view);
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflates the layout for this fragment
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.dish, parent, false);
+        CategoryViewHolder holder = new CategoryViewHolder(view);
         return holder;
     }
 
-
-
+    // Adds data to the layout elements defined in ViewHolder through Holder
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.etiquetaNom.setText(array_noms.get(position));
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int i) {
+        // Bundle properties
+        Bundle bundle = new Bundle();
+        DishesListFragment dishesListFragment = new DishesListFragment();
+        // Sets text inside TextViews
+        Category category = arrayCategories.get(i);
+        holder.name.setText(category.getName());
+
+        // Adds item object to bundle and sent to Item Details fragments
+        bundle.putSerializable("Category", category);
+        dishesListFragment.setArguments(bundle);
+
+        // When user clicks on item, will navigation to item details fragment
+        holder.itemView.setOnClickListener(v -> {
+            AppCompatActivity app = (AppCompatActivity) v.getContext();
+            app.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, dishesListFragment, "Dish").commit();
+        });
     }
 
+    // Counts how many items will iterate to display a list
     @Override
-    public int getItemCount() {
-        return array_noms.size();
-    }
+    public int getItemCount() { return arrayCategories.size(); }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView txtNom;
-        TextView txtDescripcio;
-        TextView txtPreu;
-        ImageView imgPlat;
-
-        public ViewHolder(@NonNull View itemView) {
+    // Initializes Layout properties that will link with RecyclerView (through Holder)
+    public class CategoryViewHolder extends RecyclerView.ViewHolder{
+        TextView name;
+        public CategoryViewHolder(@NonNull View itemView) {
             super(itemView);
-            txtNom = itemView.findViewById(R.id.userName);
+            name = itemView.findViewById(R.id.lbl_nameCategoryList);
         }
     }
+
 }
