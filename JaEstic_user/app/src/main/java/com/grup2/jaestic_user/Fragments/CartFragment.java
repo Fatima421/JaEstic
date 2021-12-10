750633e352dd615b56cbe7e3605ce0cef248fe4f
FatimaSahar <fatimasahar@iesjoandaustria.org>
2021-12-10 21:41:00 +0100

    [JS-20] Made the design for the cart screen and created correctly the recycler view adapter

--- JaEstic_user/app/src/main/java/com/grup2/jaestic_user/Fragments/CartFragment.java
+++ JaEstic_user/app/src/main/java/com/grup2/jaestic_user/Fragments/CartFragment.java
@@ -3,83 +3,85 @@ package com.grup2.jaestic_user.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.grup2.jaestic_user.Models.Category;
 import com.grup2.jaestic_user.Models.Dish;
 import com.grup2.jaestic_user.R;
+import com.grup2.jaestic_user.RecyclerViewAdapters.CartRecyclerViewAdapter;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CartFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Properties
+    private DatabaseReference databaseReference;
+    private ArrayList<Dish> arrayCartItems;
+    private ArrayList<CheckBox> checkBoxes;
+    RecyclerView recyclerView;
 
     public CartFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CartFragment newInstance(String param1, String param2) {
-        CartFragment fragment = new CartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View cartView = inflater.inflate(R.layout.fragment_cart, container, false);
+        View v = inflater.inflate(R.layout.fragment_cart, container, false);
+        CheckBox checkBox = v.findViewById(R.id.cartCheckBox);
 
-        ArrayList<Dish> arrayCart = new ArrayList<>();
-        arrayCart.add(new Dish(1, "", "Peix", "Loremipsum", "13", new Category()));
+        // Create the categories array list
+        arrayCartItems = new ArrayList<>();
+        arrayCartItems.add(new Dish(2, "", "Pizza", "", "12,90", (new Category())));
+        arrayCartItems.add(new Dish(2, "", "Pizza", "", "12,90", (new Category())));
+        arrayCartItems.add(new Dish(2, "", "Pizza", "", "12,90", (new Category())));
+        arrayCartItems.add(new Dish(2, "", "Pizza", "", "12,90", (new Category())));
+        arrayCartItems.add(new Dish(2, "", "Pizza", "", "12,90", (new Category())));
+        arrayCartItems.add(new Dish(2, "", "Pizza", "", "12,90", (new Category())));
+        arrayCartItems.add(new Dish(2, "", "Pizza", "", "12,90", (new Category())));
+        arrayCartItems.add(new Dish(2, "", "Pizza", "", "12,90", (new Category())));
 
-        TextView txtEmptyCart = cartView.findViewById(R.id.empty_view);
+        // Create the RecyclerView
+        recyclerView = v.findViewById(R.id.cartRecyclerView);
+        CartRecyclerViewAdapter adapter = new CartRecyclerViewAdapter(getContext(), arrayCartItems);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        checkBoxes = adapter.getCheckBoxes();
 
-        if(arrayCart.isEmpty()){
-            txtEmptyCart.setVisibility(View.VISIBLE);
-        }else{
-            txtEmptyCart.setVisibility(View.GONE);
-        }
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkBox.isChecked()) {
+                    for (int i = 0; i < checkBoxes.size(); i++) {
+                        CheckBox currentCheckBox = checkBoxes.get(i);
+                        currentCheckBox.setChecked(true);
+                    }
+                } else {
+                    for (int i = 0; i < checkBoxes.size(); i++) {
+                        CheckBox currentCheckBox = checkBoxes.get(i);
+                        currentCheckBox.setChecked(false);
+                    }
+                }
+            }
+        });
 
-        return cartView;
+        return v;
     }
 }
